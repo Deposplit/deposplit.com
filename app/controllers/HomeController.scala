4ed20b0c2a8578b3ee97cc582eeb158ef97f026a
Squeng <paul@squeng.com>
2026-04-14 08:33:37 +0200

    i18n-ed

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,23 +1,45 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2026 Squeng AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package controllers
 
-import javax.inject._
-import play.api._
-import play.api.mvc._
+import play.api.*
+import play.api.i18n.I18nSupport
+import play.api.mvc.*
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+import javax.inject.*
+
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject() (val controllerComponents: ControllerComponents)
+    extends BaseController
+    with I18nSupport {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+  /** Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method will be called when the application receives a `GET`
+    * request with a path of `/`.
+    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
